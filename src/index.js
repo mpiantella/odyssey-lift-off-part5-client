7cfba92c79055a3d9987e20671ad82f2b9e75ef4
Maria Lucena <mpiantella@hotmail.com>
2022-02-07 22:47:30 -0500

    update host

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache
+} from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://odyssey-certification-apollo.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
-  </ApolloProvider>,
+ReactDOM.render( <
+  ApolloProvider client = {
+    client
+  } >
+  <
+  GlobalStyles / >
+  <
+  Pages / >
+  <
+  /ApolloProvider>,
   document.getElementById('root')
 );
